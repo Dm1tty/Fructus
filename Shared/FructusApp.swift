fdcf4e1581b626ab677a69796ea5c615585c1ae3
Dzmitry M <ma.dzmitry@gmail.com>
2021-08-17 22:39:41 -0700

    Initial commit

---
+++ Shared/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Shared
+//
+//  Created by Dzmitry Matsiulka on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
